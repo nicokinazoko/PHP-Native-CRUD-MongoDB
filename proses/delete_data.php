8f5e0d49e07a913b9bd88374dc6d8aa6b08bf4d7
nicokinazoko <38060004+nicokinazoko@users.noreply.github.com>
2021-02-08 11:54:16 +0700

    Update delete_data.php

--- proses/delete_data.php
+++ proses/delete_data.php
@@ -7,7 +7,7 @@ $nim_mahasiswa = $_GET['nim'];
 echo $nim_mahasiswa;
 
 
-$deleteMahasiswa = $collection->deleteOne(['nim' => $nim_mahasiswa]);
+$deleteMahasiswa = $collection->deleteOne(['_id' => $nim_mahasiswa]);
 printf("Deleted %d document(s)\n", $deleteMahasiswa->getDeletedCount());
 header("Location: http://localhost/bdbo_mongodb/layout/index.php");
 
